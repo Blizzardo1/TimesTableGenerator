11b6263fa8e837395b3dfa952e57304c11e58f56
Adonis Deliannis <blizzardo1@blizzeta.net>
2022-01-20 15:14:31 -0800

    Initial commit

---
+++ TimesTableGenerator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TimesTableGenerator
+{
+    internal static class Program
+    {
+        private static int[,] MakeTable(int lenX, int lenY)
+        {
+            int[,] arr = new int[lenX, lenY];
+            for (int y = 0; y < lenY; y++)
+            {
+                for (int x = 0; x < lenX; x++)
+                {
+                    arr[y, x] = (x + 1) * (y + 1);
+                }
+            }
+
+            return arr;
+        }
+        
+        private static void Main(string[] args)
+        {
+            int lenX = 100;
+            int lenY = 100;
+            
+            int[,] arr = MakeTable(lenX, lenY);
+            
+            for (int y = 0; y < lenY; y++)
+            {
+                Console.Write($"| {y + 1}");
+                for (int x = 0; x < lenX; x++)
+                {
+                    if (y == 0)
+                    {
+                        Console.Write($"| {x+1,5}");
+                        continue;
+                    }
+                    Console.Write($"| {arr[y,x],5}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
